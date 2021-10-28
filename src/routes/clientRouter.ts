ed683a8f54fa370bee949c8ca3688b857e56bd27
jmassuia <massuia1507@gmail.com>
2021-10-28 00:16:44 -0300

    Protect middleware

--- src/routes/clientRouter.ts
+++ src/routes/clientRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/AuthController';
 import { ClientController } from '../controllers/ClientController';
-
+import { Protect } from '../middleware/Protect';
 const router = Router();
 
-router.get('/',new ClientController().index);
+router.get('/',new Protect().execute,new ClientController().index);
 router.post('/',new ClientController().create);
 
 router.post('/login',new AuthController().login)
